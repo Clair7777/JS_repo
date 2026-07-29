492fb9f22fc64493b118910512e8c151434a9533
Davydova Svetlana <svetach7777@bk.ru>
2026-07-29 18:52:57 +0300

    мой первый коммит

---
+++ script.js
@@ -0,0 +1,16 @@
+alert("Hello world!");
+console.log("Hello console!"); // comm
+
+/*
+hkhkhhk
+khkhkkh
+jjhhkkhk
+*/
+
+// const myFirstvar = 1;
+
+// const myFunc = () => {
+//   console.log(myFirstvar);
+// };
+
+// myFunc()
