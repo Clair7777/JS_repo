780d37c8956863d8a5a347927dc6ebdb797d587d
Davydova Svetlana <svetach7777@bk.ru>
2026-07-30 22:04:55 +0300

    создание ветки lesson02 и создание переменных в проекте, вывод в консоль

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-let title = 1;
-let screens = 2;
-let screenPrice = 3;
-let rollback = 4;
-let fullPrice = 5;
-let adaptive = 6;
-alert("Hello world!");
-console.log("Hello console!");
+let title = "JS_project";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 1000;
+let rollback = 77;
+let fullPrice = 15000;
+let adaptive = true;
+
+console.log(typeof title + ", " + typeof fullPrice + ", " + typeof adaptive);
+console.log(screens.length);
+console.log(
+  "Стоимость верстки экранов " + screenPrice + " рублей/долларов/гривен/юаней",
+);
+console.log(
+  "Стоимость разработки сайта " + fullPrice + " рублей/долларов/гривен/юаней",
+);
+
+let lowerScreens = screens.toLowerCase();
+let arr = lowerScreens.split(", ");
+console.log(arr);
+
+console.log(fullPrice * (rollback / 100));
