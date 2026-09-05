2f1998f3b5a86fbbd0ff35db9e8ef58327ebeda9
Davydova Svetlana <svetach7777@bk.ru>
2026-09-05 22:01:08 +0300

    lesson11 finish

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
+"use strict";
 
+const btn = document.getElementById("btn");
+const inputColor = document.getElementById("text");
+const square = document.getElementById("square");
+const span = document.getElementById("text-span");
+const btnE = document.getElementById("e_btn");
+const range = document.getElementById("range");
+const circle = document.getElementById("circle");
 
+btn.onclick = function () {
+  square.style.backgroundColor = inputColor.value;
+};
 
+btnE.onclick = function () {
+  btnE.style.display = "none";
+};
 
-
+range.oninput = function () {
+  circle.style.height = `${range.value}%`;
+  circle.style.width = `${range.value}%`;
+};
